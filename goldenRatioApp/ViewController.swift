958901d0e90fc3859280aac5b531d7f4a4a3ec21
Swamita <swamita2001@gmail.com>
2020-09-08 10:03:51 +0530

    Initial Commit

---
+++ goldenRatioApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goldenRatioApp
+//
+//  Created by Swamita on 08/09/20.
+//  Copyright © 2020 Swamita Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
