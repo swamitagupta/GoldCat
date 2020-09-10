cbaba48bbe4ff19e7c649ae44684a4e020cdf547
Swamita <swamita2001@gmail.com>
2020-09-10 15:08:30 +0530

    Code image picker

--- goldenRatioApp/Controller/ImplementationViewController.swift
+++ goldenRatioApp/Controller/ImplementationViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import Vision
 
-class ImplementationViewController: UIViewController {
+class ImplementationViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.originalImage] as? UIImage {
+            
+            imageView.image = image
+            imagePicker.dismiss(animated: true, completion: nil)
+            guard let ciImage = CIImage(image: image) else {
+                fatalError("couldn't convert uiimage to CIImage")
+            }
+        }
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
