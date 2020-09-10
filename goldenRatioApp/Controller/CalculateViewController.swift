91867257b9b19db6698ef03a9b7403d739e36e84
Swamita <swamita2001@gmail.com>
2020-09-10 23:29:45 +0530

    Hide keyboard when tapped

--- goldenRatioApp/Controller/CalculateViewController.swift
+++ goldenRatioApp/Controller/CalculateViewController.swift
@@ -18,7 +18,7 @@ class CalculateViewController: UIViewController {
     var greaterNumber = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.hideKeyboardWhenTappedAround() 
         // Do any additional setup after loading the view.
     }
     
@@ -33,15 +33,16 @@ class CalculateViewController: UIViewController {
         }
         
     }
-    
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
     }
-    */
 
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
