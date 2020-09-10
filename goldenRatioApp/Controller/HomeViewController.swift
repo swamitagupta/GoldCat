ca85780dc5e76657b8d944b932582f527036f49c
Swamita <swamita2001@gmail.com>
2020-09-10 21:42:42 +0530

    Add illustration

--- goldenRatioApp/Controller/HomeViewController.swift
+++ goldenRatioApp/Controller/HomeViewController.swift
@@ -12,25 +12,29 @@ class HomeViewController: UIViewController {
 
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var textView: UITextView!
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var image: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         textView.text = theory.text
-        imageView.isHidden = true
+        textLabel.text = illustration.text
+        image.isHidden = true
+        textLabel.isHidden = true
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func indexChanged(_ sender: Any) {
         switch segmentedControl.selectedSegmentIndex
         {
         case 0:
-            textView.text = theory.text
-            imageView.isHidden = true
+            textView.isHidden = false
+            textLabel.isHidden = true
+            image.isHidden = true
         case 1:
-            textView.text = illustration.text
-            imageView.isHidden = false
+            textView.isHidden = true
+            textLabel.isHidden = false
+            image.isHidden = false
         default:
             break
         }
