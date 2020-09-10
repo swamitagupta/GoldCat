f71b42a28db79b570b240d3025a552b9349faf04
Swamita <swamita2001@gmail.com>
2020-09-10 14:57:13 +0530

    Complete Collection View

--- goldenRatioApp/Controller/ApplicationViewController.swift
+++ goldenRatioApp/Controller/ApplicationViewController.swift
@@ -8,7 +8,20 @@
 
 import UIKit
 
-class ApplicationViewController: UIViewController {
+class ApplicationViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let fields = ["Design", "Architecture", "Engineering", "Art", "Music", "Nature", "Math", "Pyramids"]
+    let icons = [UIImage(systemName: "pencil.and.outline")
+    ,UIImage(systemName: "house")
+    ,UIImage(systemName: "gear")
+    ,UIImage(systemName: "paintbrush.fill")
+    ,UIImage(systemName: "music.note.list")
+    ,UIImage(systemName: "globe")
+    ,UIImage(systemName: "plus.slash.minus")
+    ,UIImage(systemName: "triangle.lefthalf.fill") ]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +29,32 @@ class ApplicationViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fields.count
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CVCell", for: indexPath) as! CollectionViewCell
+        cell.contentView.backgroundColor = UIColor(named: "Golden")
+        cell.titleLabel.textColor = UIColor.black
+        cell.titleLabel.text = fields[indexPath.item]
+        cell.icon.image = icons[indexPath.item]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // Compute the dimension of a cell for an NxN layout with space S between
+        // cells.  Take the collection view's width, subtract (N-1)*S points for
+        // the spaces between the cells, and then divide by N to find the final
+        // dimension for the cell's width and height.
+        let dim = collectionView.bounds.width/2.1
+        return CGSize(width: dim, height: dim)
+    }
+
 
 }
---
+++ goldenRatioApp/Controller/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  goldenRatioApp
+//
+//  Created by Swamita on 10/09/20.
+//  Copyright © 2020 Swamita Gupta. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+}
