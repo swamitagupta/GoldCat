3d9dfd52d7e03a45d1d019712d1faec2a402aab9
Swamita <swamita2001@gmail.com>
2020-09-10 19:41:15 +0530

    Implement Vision Framework

--- goldenRatioApp/Controller/ImplementationViewController.swift
+++ goldenRatioApp/Controller/ImplementationViewController.swift
@@ -8,33 +8,165 @@
 
 import UIKit
 import Vision
+import ImageIO
 
-class ImplementationViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ImplementationViewController: UIViewController, UINavigationControllerDelegate {
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var initialImageView: UIImageView!
+    @IBOutlet weak var finalImageView: UIImageView!
+    
     let imagePicker = UIImagePickerController()
+    
+    var image : CIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
     }
     
+    lazy var detectRectangleRequest: VNDetectRectanglesRequest = {
+        return VNDetectRectanglesRequest(completionHandler:self.handleRectangles)
+    }()
+    
+    lazy var detectTextRequest: VNDetectTextRectanglesRequest = {
+        let textRequest = VNDetectTextRectanglesRequest(completionHandler: self.handleTextIdentifiaction)
+        textRequest.reportCharacterBoxes = true
+        return textRequest
+    }()
+    
+    lazy var detectFaceRequest : VNDetectFaceRectanglesRequest = {
+        let faceRequest = VNDetectFaceRectanglesRequest(completionHandler:self.handleFaceDetection)
+        return faceRequest
+    }()
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
+extension ImplementationViewController: UIImagePickerControllerDelegate {
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
-        if let image = info[.originalImage] as? UIImage {
-            
-            imageView.image = image
-            imagePicker.dismiss(animated: true, completion: nil)
-            guard let ciImage = CIImage(image: image) else {
-                fatalError("couldn't convert uiimage to CIImage")
+        guard let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            else { fatalError("Failed to load image!") }
+        guard let ciImage = CIImage(image: uiImage)
+            else { fatalError("Failed to convert UIImage to CIImage!") }
+        
+        image = ciImage.oriented(forExifOrientation: Int32(uiImage.imageOrientation.rawValue))
+        initialImageView.image = uiImage
+                
+        let handler = VNImageRequestHandler(ciImage: ciImage, orientation: CGImagePropertyOrientation(rawValue: CGImagePropertyOrientation.RawValue(Int32(uiImage.imageOrientation.rawValue)))!)
+                
+                DispatchQueue.global(qos: .userInteractive).async {
+                    do {
+                        try handler.perform([self.detectRectangleRequest,
+                                             self.detectTextRequest,
+                                             self.detectFaceRequest])
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+    
+    
+    func Outline(withColor : UIColor) -> UIView {
+        let view = UIView()
+        view.layer.borderColor = withColor.cgColor
+        view.layer.borderWidth = 2
+        view.backgroundColor = UIColor.clear
+        return view
+    }
+    
+    func transform(fromRect: CGRect , toViewRect :UIView) -> CGRect {
+        
+        var toRect = CGRect()
+        toRect.size.width = fromRect.size.width * toViewRect.frame.size.width
+        toRect.size.height = fromRect.size.height * toViewRect.frame.size.height
+        toRect.origin.y =  (toViewRect.frame.height) - (toViewRect.frame.height * fromRect.origin.y )
+        toRect.origin.y  = toRect.origin.y -  toRect.size.height
+        toRect.origin.x =  fromRect.origin.x * toViewRect.frame.size.width
+        
+        return toRect
+    }
+    
+    func handleTextIdentifiaction (request: VNRequest, error: Error?) {
+        
+        guard let observations = request.results as? [VNTextObservation]
+            else { print("Unexpected result type from VNTextObservation")
+                return
+            }
+        guard observations.first != nil else {
+            return
+        }
+        DispatchQueue.main.async {
+            self.finalImageView.subviews.forEach({ (s) in
+                s.removeFromSuperview()
+            })
+            for box in observations {
+                guard let chars = box.characterBoxes else {
+                    print("no char values found")
+                    return
+                }
+                for char in chars
+                {
+                    let view = self.Outline(withColor: UIColor.red)
+                    view.frame = self.transform(fromRect: char.boundingBox, toViewRect: self.finalImageView)
+                    self.finalImageView.image = self.initialImageView.image
+                    self.finalImageView.addSubview(view)
+                }
+            }
+        }
+        
+    }
+    
+    
+    func handleFaceDetection (request: VNRequest, error: Error?) {
+        guard let observations = request.results as? [VNFaceObservation]
+            else { print("unexpected result type from VNFaceObservation")
+                return }
+        guard observations.first != nil else {
+            return
+        }
+        // Show the pre-processed image
+        DispatchQueue.main.async {
+            self.finalImageView.subviews.forEach({ (s) in
+                s.removeFromSuperview()
+            })
+            for face in observations
+            {
+                let view = self.Outline(withColor: UIColor.red)
+                view.frame = self.transform(fromRect: face.boundingBox, toViewRect: self.self.finalImageView)
+                self.finalImageView.image = self.initialImageView.image
+                self.finalImageView.addSubview(view)
             }
         }
     }
     
-    @IBAction func cameraTapped(_ sender: Any) {
+    func handleRectangles(request: VNRequest, error: Error?) {
         
-        imagePicker.sourceType = .photoLibrary
-        imagePicker.allowsEditing = false
-        present(imagePicker, animated: true, completion: nil)
+        guard let observations = request.results as? [VNRectangleObservation]
+            else { print("unexpected result type from VNDetectRectanglesRequest")
+                    return
+        }
+        guard observations.first != nil else {
+            return
+        }
+        // Show the pre-processed image
+        DispatchQueue.main.async {
+            self.finalImageView.subviews.forEach({ (s) in
+                s.removeFromSuperview()
+            })
+            for rect in observations
+            {
+                let view = self.Outline(withColor: UIColor.cyan)
+                view.frame = self.transform(fromRect: rect.boundingBox, toViewRect: self.finalImageView)
+                self.finalImageView.image = self.initialImageView.image
+                self.finalImageView.addSubview(view)
+            }
+        }
     }
 }
